91eed93140325d0d18e3cd8e486d3aaee20fbbfa
quangng01 <nguyenduyquangk0@gmail.com>
2023-02-27 22:53:48 +0700

    set up JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,26 +1,38 @@
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
-import {BadRequestError } from "../errors/index.js"
+import { BadRequestError } from "../errors/index.js";
 
 const register = async (req, res, next) => {
-    const {name, email, password} = req.body
-    if(!name || !email || !password){
-      throw new BadRequestError("Vui lòng nhập đủ thông tin!")
-    }
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    throw new BadRequestError("Vui lòng nhập đủ thông tin!");
+  }
 
-    const userAlreadyExists = await User.findOne({email})
-    if(userAlreadyExists){
-      throw new BadRequestError("Email đã tồn tại!")
-    }
-    const user = await User.create(req.body);
-    res.status(StatusCodes.OK).json({ user });
-}
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError("Email đã tồn tại!");
+  }
+  const user = await User.create(req.body);
+  const token = user.createJWT();
+  res
+    .status(StatusCodes.OK)
+    .json({
+      user: {
+        email: user.email,
+        lastName: user.lastName,
+        location: user.location,
+        name: user.name,
+      },
+      token,
+      location: user.location,
+    });
+};
 const login = async (req, res) => {
   res.send("login user");
 };
 const updatedUser = async (req, res) => {
   res.send("updated user");
-  User.findOneAndUpdate
+  User.findOneAndUpdate;
 };
 
 export { register, login, updatedUser };
