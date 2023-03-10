7793612684f9aa49c5d3247e0748f0ee50056828
QuangDev69 <nguyenduyquangk0@gmail.com>
2023-03-10 14:37:32 +0700

    edit job request

--- clientss/src/context/reducer.js
+++ clientss/src/context/reducer.js
@@ -17,6 +17,10 @@ import {
   GET_JOB_BEGIN,
   GET_JOB_SUCCESS,
   SET_EDIT_JOB,
+  DELETE_JOB_BEGIN,
+  EDIT_JOB_BEGIN,
+  EDIT_JOB_SUCCESS,
+  EDIT_JOB_ERROR,
 } from './actions'
 import { initialState } from './appContext'
 
@@ -235,6 +239,35 @@ const reducer = (state, action) => {
       jobLocation,
       jobType,
       status,
+    }
+  }
+
+  if (action.type === DELETE_JOB_BEGIN) {
+    return { ...state, isLoading: true }
+  }
+
+  if (action.type === EDIT_JOB_BEGIN) {
+    return {
+      ...state,
+      isLoading: false,
+    }
+  }
+  if (action.type === EDIT_JOB_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: 'success',
+      alertText: 'Job updated',
+    }
+  }
+  if (action.type === EDIT_JOB_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: 'danger',
+      alertText: action.payload.msg,
     }
   }
 
